d36ddc6bcf53e31c9edd7459cb2e5552537f182b
Mikhail Kravchenko <mtkravchenko@gmail.com>
2026-05-19 16:44:28 -0300

    version 27

--- lm1/own.lm1.h
+++ lm1/own.lm1.h
@@ -6,7 +6,21 @@
 typedef void (*LmOwnDestroyFields)(void *object);
 typedef void (*LmOwnDelete)(void *object);
 
+typedef struct LmOwnPtrStack {
+    void **items;
+    size_t count;
+    size_t capacity;
+    LmOwnDelete delete_item;
+} LmOwnPtrStack;
+
 void lm_own_delete(void *object, LmOwnDestroyFields destroy_fields);
 void lm_own_pointer_array_delete(void **items, size_t count, LmOwnDelete delete_item);
+void lm_own_ptr_stack_init(LmOwnPtrStack *stack, LmOwnDelete delete_item);
+void lm_own_ptr_stack_destroy(LmOwnPtrStack *stack);
+int lm_own_ptr_stack_push(LmOwnPtrStack *stack, void *item);
+void *lm_own_ptr_stack_pop(LmOwnPtrStack *stack);
+void *lm_own_ptr_stack_at(const LmOwnPtrStack *stack, size_t index);
+void *lm_own_ptr_stack_top(const LmOwnPtrStack *stack);
+void lm_own_ptr_stack_truncate(LmOwnPtrStack *stack, size_t count);
 
 #endif
