d6c8bd6cf9911b6afbd67e3e90fb4442225295d7
kravchenko <mtkravchenko@gmail.com>
2026-07-13 17:32:11 -0300

    Read field chain types from source rows

--- lm1/build/trans.lm1.c
+++ lm1/build/trans.lm1.c
@@ -2787,6 +2787,7 @@ static const LmP0Field * lm_trans_call_body_first_field(const LmP0Structure *bod
 static int lm_trans_call_args_layout_signature(LmOwnPtrStack *segments, const LmP0Field *field, const LmTransSymbol *callee);
 static int lm_trans_call_args_layout(LmOwnPtrStack *segments, const LmP0Structure *body, const LmTransSymbol *callee);
 static int lm_trans_head_next_field_chain_segment(const LmP0Text *head, size_t *offset, LmP0Text *out_segment);
+static int lm_trans_layout_field_class_resolve(const LmP0Text *owner, const LmP0Text *field_name, const LmTransNamespace *namespace_, const char **out_class);
 static int lm_trans_callable_expression_descriptor_name(const LmP0Text *head, const LmTransNamespace *namespace_, LmP0Text *out_descriptor_name);
 static int lm_trans_callable_expression_signature_fill_param_names(LmTransSymbol *signature);
 static LmTransSymbol * lm_trans_callable_expression_signature_new(const LmTransNamespace *namespace_, const LmP0Text *descriptor_name);
@@ -11879,14 +11880,101 @@ static int lm_trans_head_next_field_chain_segment(const LmP0Text *head, size_t *
     return -1;
 }
 
+static int lm_trans_layout_field_class_resolve(const LmP0Text *owner, const LmP0Text *field_name, const LmTransNamespace *namespace_, const char **out_class) {
+    char *relation_name;
+    char *source_table_name;
+    const char *facade_class;
+    const char *source_class;
+    const LmTableDescriptor * source_table;
+    const LmTableRow * source_row;
+    const LmTableColumnDescriptor * source_key_column;
+    const LmTableColumnDescriptor * source_field_class_column;
+    const LmTableColumnDescriptor * source_field_index_column;
+    const LmTableColumnDescriptor * source_address_depth_column;
+    const LmTableColumnDescriptor * source_const_column;
+    const LmTableCell * source_field_class_cell;
+    const LmTableCell * source_field_index_cell;
+    size_t source_column_count;
+    int source_covered;
+    int source_usable;
+    if (out_class != 0) {
+        out_class[0] = 0;
+    }
+    if (owner == 0 || field_name == 0 || out_class == 0) {
+        return 0;
+    }
+    relation_name = lm_trans_registry_relation_name_new(owner, ".field.class");
+    if (relation_name == 0) {
+        return -1;
+    }
+    facade_class = lm_trans_namespace_registry_lookup(namespace_, field_name, relation_name);
+    lm_own_delete(relation_name, 0);
+    if (facade_class == 0) {
+        return 0;
+    }
+    out_class[0] = facade_class;
+    if (lm_trans_registry_view_parity_enabled() == 0) {
+        return 1;
+    }
+    if (namespace_ != 0 && namespace_ -> registry_identifiers != 0 && namespace_ -> registry_identifiers != lm_trans_registry->identifiers) {
+        return 1;
+    }
+    source_table_name = lm_trans_text_copy_cstr(owner);
+    if (source_table_name == 0) {
+        return 1;
+    }
+    source_covered = 0;
+    source_table = 0;
+    source_row = lm_trans_registry_source_table_lookup_row(field_name, source_table_name, &source_table, &source_covered);
+    source_class = 0;
+    source_usable = 0;
+    if (source_row != 0) {
+        source_column_count = lm_table_descriptor_column_count(source_table);
+        source_usable = (source_column_count == 3U || source_column_count == 4U || source_column_count == 5U) && lm_table_row_cell_count(source_row) == source_column_count;
+        if (source_usable != 0) {
+            source_key_column = lm_table_descriptor_column_at(source_table, 0U);
+            source_field_class_column = lm_table_descriptor_column_at(source_table, 1U);
+            source_field_index_column = lm_table_descriptor_column_at(source_table, 2U);
+            source_address_depth_column = 0;
+            source_const_column = 0;
+            if (source_column_count >= 4U) {
+                source_address_depth_column = lm_table_descriptor_column_at(source_table, 3U);
+            }
+            if (source_column_count == 5U) {
+                source_const_column = lm_table_descriptor_column_at(source_table, 4U);
+            }
+            source_usable = source_key_column != 0 && source_key_column -> name != 0 && strcmp(source_key_column -> name, "class") == 0 && source_field_class_column != 0 && source_field_class_column -> name != 0 && strcmp(source_field_class_column -> name, "field.class") == 0 && source_field_index_column != 0 && source_field_index_column -> name != 0 && strcmp(source_field_index_column -> name, "field.index") == 0;
+            if (source_usable != 0 && source_address_depth_column != 0) {
+                source_usable = source_address_depth_column -> name != 0 && strcmp(source_address_depth_column -> name, "field.address-depth") == 0;
+            }
+            if (source_usable != 0 && source_const_column != 0) {
+                source_usable = source_const_column -> name != 0 && strcmp(source_const_column -> name, "field.const") == 0;
+            }
+        }
+        if (source_usable != 0) {
+            source_field_class_cell = lm_table_row_cell_at(source_row, 1U);
+            source_field_index_cell = lm_table_row_cell_at(source_row, 2U);
+            source_usable = source_field_class_cell != 0 && source_field_class_cell -> value != 0 && source_field_index_cell != 0 && source_field_index_cell -> value != 0;
+            if (source_usable != 0) {
+                source_class = source_field_class_cell -> value;
+            }
+        }
+    }
+    if (source_covered != 0 && source_usable != 0 && strcmp(source_class, facade_class) == 0 && lm_trans_registry_view_is_selected() != 0) {
+        out_class[0] = source_class;
+    }
+    lm_own_delete(source_table_name, 0);
+    return 1;
+}
+
 static int lm_trans_callable_expression_descriptor_name(const LmP0Text *head, const LmTransNamespace *namespace_, LmP0Text *out_descriptor_name) {
     const LmTransSymbol * symbol;
     LmP0Text * segment;
     LmP0Text * current_class;
-    char *relation_name;
     const char *field_class;
     size_t offset;
     int segment_status;
+    int field_class_status;
     int has_field_chain;
     int result;
     if (out_descriptor_name != 0) {
@@ -11931,14 +12019,12 @@ static int lm_trans_callable_expression_descriptor_name(const LmP0Text *head, co
             result = -1;
             break;
         }
-        relation_name = lm_trans_registry_relation_name_new(current_class, ".field.class");
-        if (relation_name == 0) {
+        field_class_status = lm_trans_layout_field_class_resolve(current_class, segment, namespace_, &field_class);
+        if (field_class_status < 0) {
             result = -1;
             break;
         }
-        field_class = lm_trans_namespace_registry_lookup(namespace_, segment, relation_name);
-        lm_own_delete(relation_name, 0);
-        if (field_class == 0) {
+        if (field_class_status == 0) {
             fprintf(stderr, "trans L2 type error: field-chain callable head \"%.*s\" has no field \"%.*s\" on class \"%.*s\"\n", (((int)head -> length)), head -> data, (((int)segment -> length)), segment -> data, (((int)current_class -> length)), current_class -> data);
             result = -1;
             break;
@@ -12433,10 +12519,10 @@ static int lm_trans_printf_arg_class_from_field_chain(const LmP0Text *atom, cons
     const LmTransSymbol * symbol;
     LmP0Text * segment;
     LmP0Text * current_class;
-    char *relation_name;
     const char *field_class;
     size_t offset;
     int segment_status;
+    int field_class_status;
     int result;
     if (out_class != 0) {
         out_class->data = "";
@@ -12480,14 +12566,8 @@ static int lm_trans_printf_arg_class_from_field_chain(const LmP0Text *atom, cons
             result = 0;
             break;
         }
-        relation_name = lm_trans_registry_relation_name_new(current_class, ".field.class");
-        if (relation_name == 0) {
-            result = 0;
-            break;
-        }
-        field_class = lm_trans_namespace_registry_lookup(namespace_, segment, relation_name);
-        lm_own_delete(relation_name, 0);
-        if (field_class == 0) {
+        field_class_status = lm_trans_layout_field_class_resolve(current_class, segment, namespace_, &field_class);
+        if (field_class_status <= 0) {
             result = 0;
             break;
         }
